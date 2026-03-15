ae3a5bb23cf3291e815ec2998297cca16ecf1c8c
lkotlus <logan.kotlus@gmail.com>
2026-03-15 00:28:13 -0400

    VPN and Ansible need fixed.

--- playbooks/scripts/setup_flask_custom_serial.sh
+++ playbooks/scripts/setup_flask_custom_serial.sh
@@ -1,42 +1,12 @@
 #!/bin/bash
 # FLASK MACHINE
 
-sudo apt update
-sudo apt install -y curl
-
 # install app.py dependencies
+sudo apt update
 sudo apt install -y python3-pip mysql-client-core-8.0
-pip install flask
-pip install mysql-connector-python 
+pip install flask mysql-connector-python
 
-# install app.py and run
-cat <<EOF > app.py 
-from flask import Flask
-import mysql.connector
-import os
-import time
-
-app = Flask(__name__)
-
-IP_ADDR = os.getenv("IP_ADDR")
-
-db = None
-while db is None:
-    try:
-        db = mysql.connector.connect(host=IP_ADDR, username='flask', password = 'sudo', database='users')
-    except mysql.connector.Error as e:
-        print(f"Waiting for MySQL: {e}")
-        time.sleep(2)
-
-cursor = db.cursor()
-
-@app.route('/')
-def hello_world():
-    cursor.execute('SELECT * FROM users')
-    result = cursor.fetchall()
-    return f'<p>{result}</p>'
-EOF
-
+# Create a service file
 cat <<EOF > /etc/systemd/system/flask.service
 [Unit]
 Description=Flask App
@@ -45,7 +15,6 @@ After=network.target
 [Service]
 User=ubuntu
 WorkingDirectory=/home/ubuntu
-Environment="IP_ADDR=${IP_ADDR}"
 ExecStart=python3 -m flask run --host=0.0.0.0
 Restart=always
 
@@ -53,6 +22,7 @@ Restart=always
 WantedBy=multi-user.target
 EOF
 
+# Enable and start the service
 systemctl daemon-reload
 systemctl enable flask
 systemctl start flask
